8ac3ba47cd06cc2c42666d0f796eb3cf14d98059
Ustinov Andrey <andreeicandrej00@gmail.com>
2021-10-02 09:11:38 +0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,28 +1,37 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StyleSheet,View , Text} from 'react-native';
+import {SafeAreaView } from 'react-native-safe-area-context';
 
-import useCachedResources from './src/hooks/useCachedResources';
-import useColorScheme from './src/hooks/useColorScheme';
-import Navigation from './src/config/navigation';
-import { Provider } from 'react-redux';
-import { store } from './src/config/redux';
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
+      <View style={styles.container}>
+        <Text style={styles.text}>
+      Hello
+        </Text>
+        <StatusBar />
+      </View>
+    </SafeAreaView>
+  );
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <Provider store={store}>
-        <SafeAreaProvider>
-          <Navigation colorScheme={colorScheme} />
-          <StatusBar />
-        </SafeAreaProvider>
-      </Provider>
-    );
-  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'blue'
+  },
+  text: {
+    color: 'white',
+    fontSize: 24
+  }
+
+});
+
+
+
